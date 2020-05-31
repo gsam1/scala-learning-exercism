d540ba4938c7db47f85c1609ddb0c344c146ccc7
Georgi Samardzhiev <gsamardjiev@gmail.com>
2020-05-31 17:22:18 +0300

    Update GradeSchool.scala

--- grade-school/src/main/scala/GradeSchool.scala
+++ grade-school/src/main/scala/GradeSchool.scala
@@ -1,12 +1,18 @@
 class School {
   type DB = Map[Int, Seq[String]]
 
-  def add(name: String, g: Int) = ???
+  private val _db = scala.collection.mutable.Map.empty[Int, Seq[String]]
 
-  def db: DB = ???
+  def db: Map[Int, Seq[String]] = _db.toMap
 
-  def grade(g: Int): Seq[String] = ???
+  def add(name: String, g: Int): Unit =
+    _db.update(g, db.getOrElse(g, Seq.empty[String]) :+ name)
 
-  def sorted: DB = ???
+  def grade(g: Int): Seq[String] = _db.getOrElse(g, Seq.empty[String])
+
+  def sorted: Map[Int, Seq[String]] =
+    _db.toSeq
+      .sortBy(_._1)
+      .map(a => (a._1, a._2.sorted))
+      .toMap
 }
-
